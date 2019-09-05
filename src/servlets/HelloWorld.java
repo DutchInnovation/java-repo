4528ca4e0703a6f8ed71f6e017b1ff12efc8568a
leonvk <leonvk2000@gmail.com>
2019-09-05 10:09:09 +0200

    Update HelloWorld.java

--- src/servlets/HelloWorld.java
+++ src/servlets/HelloWorld.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/HelloWorld")
+@WebServlet("/blablabla")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
