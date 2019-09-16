d176af39ec3b101dc195213f99543db48e05c36e
leonvk <leonvk2000@gmail.com>
2019-09-16 12:45:08 +0200

    controlers and views opdracht
    samen met richard gemaakt.

---
+++ src/controllers/MyFirstController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstController
+ */
+@WebServlet("/test-controller")
+public class MyFirstController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFirstController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 request.setAttribute("cat", "Mimi");
+    	 request.getRequestDispatcher("/WEB-INF/views/myFirstView.jsp").forward(request, response);
+    	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
